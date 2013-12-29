96879d996e468015af8bd5cf9edb7b02ff67a560
Matt Lebrun <lebrun.matt@gmail.com>
2013-12-29 22:28:14 +0800

    Added descriptions and study drill in the code

--- ex1.py
+++ ex1.py
@@ -1,3 +1,7 @@
+# Exercise 1: A good first program
+
+# Copy these lines as is:
+
 print('Hello World!')
 print('Hello Again')
 print('I like typing this.')
@@ -5,3 +9,9 @@ print('This is fun.')
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+
+# Study Drills
+# 1. Make you script print another line.
+# 2. Make your script print only one the lines
+# 3. Put a '#' (octothorpe) character at the beginning of a line.
